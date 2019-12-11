a1c8ac745df1dd4c61f82b8b2a13bdb5bdd35137
Omar Jbara <omar.jbara2@gmail.com>
2019-12-11 12:26:17 +0100

    - Removed `getInitialProps` from _app
    It allows NextJS to automatically optimize static optimization (see https://nextjs.org/learn/excel/automatic-static-optimization)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,6 @@ import App from 'next/app'
 import AppNav from '~/components/layout/AppNav'
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render() {
     const { Component, pageProps } = this.props
 
