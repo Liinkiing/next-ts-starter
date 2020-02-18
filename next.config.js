cab5bfb75109c5a8680401a020388e43a302d8f9
Omar Jbara <omar.jbara2@gmail.com>
2020-02-18 02:16:49 +0100

    - Added support for font loader and images

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
-const nextEnv = require('next-env')
 const dotenvLoad = require('dotenv-load')
+
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+const withNextEnv = require('next-env')()
+const withFonts = require('next-fonts')
 
 dotenvLoad()
 
-const withNextEnv = nextEnv()
-
-module.exports = withNextEnv({
+module.exports = withPlugins([withNextEnv, withFonts, withImages], {
   onDemandEntries: {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
