d9915dc4d410afea0ff7c17dd05be1fbf114f1d3
Omar Jbara <omar.jbara2@gmail.com>
2020-02-18 01:18:56 +0100

    - Fixed AppNav type

--- src/components/layout/AppNav.tsx
+++ src/components/layout/AppNav.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-import { NextPage } from 'next'
 
-const AppNav: NextPage = () => (
+const AppNav: React.FC = () => (
   <nav>
     <ul>
       <li>
