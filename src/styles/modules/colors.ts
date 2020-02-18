2313840ef0725898992b4bda133cd01fbcaedb8f
Omar Jbara <omar.jbara2@gmail.com>
2020-02-18 01:11:55 +0100

    - Changed default export from array to object

--- src/styles/modules/colors.ts
+++ src/styles/modules/colors.ts
@@ -4,4 +4,4 @@ export const green = '#7ec575'
 export const lightGray = '#e8ebf3'
 export const white = '#f8f8f8'
 
-export default [dark, green, red, lightGray, white]
+export default { dark, green, red, lightGray, white }
