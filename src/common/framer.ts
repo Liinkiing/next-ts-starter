c8d6964d683f305dbe1697d75c79d310e3df291c
Omar Jbara <omar.jbara2@gmail.com>
2020-08-15 03:07:12 +0200

    - Modified default variants

--- src/common/framer.ts
+++ src/common/framer.ts
@@ -3,13 +3,12 @@ import { Variants } from 'framer-motion'
 export const ease = [0.48, 0.15, 0.25, 0.96]
 
 export const DefaultRouterPageVariants: Variants = {
-  exit: { opacity: 0, scale: 0.9, x: -10, transition: { duration: 0.2, ease } },
+  exit: { opacity: 0, x: -10, transition: { duration: 0.15, ease } },
   enter: {
     opacity: 1,
-    scale: 1,
     x: 0,
     transition: {
-      duration: 0.2,
+      duration: 0.15,
       ease,
     },
   },
