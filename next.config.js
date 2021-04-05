defba66c1d15b090946eafe4b0edf15f6362ef51
Omar Jbara <omar.jbara2@gmail.com>
2021-04-05 18:02:30 +0200

    - Upgraded to Next@10.1 and Webpack@5

--- next.config.js
+++ next.config.js
@@ -10,13 +10,17 @@ const withFonts = require('next-fonts')
 dotenvLoad()
 
 module.exports = withPlugins([withFonts, withImages], {
+  future: {
+    webpack5: true,
+  },
   onDemandEntries: {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
   },
   webpack(config) {
-    config.node = {
-      fs: 'empty',
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
     }
     config.plugins.push(
       new webpack.DefinePlugin({
